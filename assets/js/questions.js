2b8ad3577b4789d68da9f427d50470666496dba2
Emad Saeed <eaf.saeed@gmail.com>
2022-12-02 23:56:03 +0000

    questions data

---
+++ assets/js/questions.js
@@ -0,0 +1,107 @@
+var data = [
+    {
+        question: 'Inside which HTML element do we put the JavaScript?',
+        choices: [
+            {
+                answer: '<javascript>',
+                correct: false,
+            },
+            {
+                answer: ' <js>',
+                correct: false,
+            },
+            {
+                answer: '<scripting>',
+                correct: false,
+            },
+            {
+                answer: '<script>',
+                correct: true,
+            },
+        ]
+    },
+    {
+        question: 'How do you write "Hello World" in an alert box?',
+        choices: [
+            {
+                answer: 'msgBox("Hello World");',
+                correct: false,
+            },
+            {
+                answer: 'alert("Hello World");',
+                correct: true,
+            },
+            {
+                answer: 'alertBox("Hello World");',
+                correct: false,
+            },
+            {
+                answer: 'msg("Hello World");',
+                correct: false,
+            },
+        ]
+    },
+    {
+        question: 'How to write an IF statement in JavaScript?',
+        choices: [
+            {
+                answer: 'if (i == 5)',
+                correct: true,
+            },
+            {
+                answer: 'if i = 5 then',
+                correct: false,
+            },
+            {
+                answer: 'if i == 5 then',
+                correct: false,
+            },
+            {
+                answer: 'if i = 5',
+                correct: false,
+            },
+        ]
+    },
+    {
+        question: 'What is the correct way to write a JavaScript array?',
+        choices: [
+            {
+                answer: 'var colors = ["red", "green", "blue"]',
+                correct: true,
+            },
+            {
+                answer: 'var colors = "red", "green", "blue"',
+                correct: false,
+            },
+            {
+                answer: 'var colors = 1 = ("red"), 2 = ("green"), 3 = ("blue")',
+                correct: false,
+            },
+            {
+                answer: 'var colors = (1:"red", 2:"green", 3:"blue")',
+                correct: false,
+            },
+        ]
+    },
+    {
+        question: 'How do you round the number 7.25, to the nearest integer?',
+        choices: [
+            {
+                answer: 'rnd(7.25)',
+                correct: false,
+            },
+            {
+                answer: 'round(7.25)',
+                correct: false,
+            },
+            {
+                answer: 'Math.round(7.25)',
+                correct: true,
+            },
+            {
+                answer: 'Math.rnd(7.25)',
+                correct: false,
+            },
+        ]
+    },
+]
